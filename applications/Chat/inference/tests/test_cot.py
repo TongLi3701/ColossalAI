01441b7e7794b9f0eee46cbc37f4815d6431ce95
Tong Li <tong.li352711588@gmail.com>
2023-04-07 17:44:37 +0800

    add template file

---
+++ applications/Chat/inference/tests/test_cot.py
@@ -0,0 +1,18 @@
+"""
+Test inference with / without CoT
+"""
+
+import unittest
+
+
+
+class TestInference(unittest.TestCase):
+    def test_inference_without_cot(self):
+        pass
+
+    def test_inference_with_cot(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
