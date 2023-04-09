e25c98a405013f0017bb86ad1bb85a69c63d22f8
Tong Li <tong.li352711588@gmail.com>
2023-04-09 19:35:54 +0800

    add sample code

--- applications/Chat/inference/tests/test_cot.py
+++ applications/Chat/inference/tests/test_cot.py
@@ -4,14 +4,44 @@ Test inference with / without CoT
 import sys
 import unittest
 sys.path.append('..')
+
+import torch
+from transformers import LlamaForCausalLM
 from transformers import AutoTokenizer
 from utils import ChatPromptProcessor, Dialogue
 
+CONTEXT = 'Below is an instruction that describes a task. Write a response that appropriately completes the request. Do not generate new instructions.'
+tokenizer = AutoTokenizer.from_pretrained("/data/scratch/LLaMa-7B")
+USE_8BIT = False
 
+samples = [
+    ([
+        Dialogue(
+            instruction='Who is the best player in the history of NBA?',
+            response=
+            'The best player in the history of the NBA is widely considered to be Michael Jordan. He is one of the most successful players in the league, having won 6 NBA championships with the Chicago Bulls and 5 more with the Washington Wizards. He is a 5-time MVP, 1'
+        ),
+        Dialogue(instruction='continue this talk', response=''),
+    ], 128
+    ),
+]
 
 class TestInference(unittest.TestCase):
     def testInferenceWithoutCot(self):
-        pass
+        processor = ChatPromptProcessor(tokenizer, CONTEXT, 256)
+        model = LlamaForCausalLM.from_pretrained(
+                    "/data/scratch/LLaMa-7B",
+                    load_in_8bit=USE_8BIT,
+                    torch_dtype=torch.float16,
+                    device_map="auto",
+                )
+        model.eval()
+        for history, max_new_tokens in samples:
+            prompt = processor.preprocess_prompt(history, max_new_tokens)
+            inputs = {k: v.cuda() for k, v in tokenizer(prompt, return_tensors="pt").items()}
+            generate_ids = model.generate(**inputs, max_length = 256)
+            output = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+            print(output)
 
     def testInferenceWithCot(self):
         pass
